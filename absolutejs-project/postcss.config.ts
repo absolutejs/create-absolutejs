a1de8bf5d57e896eb9e1d801f6fbd908a42e96fa
Alex Kahn <alexkahndev@gmail.com>
2025-05-15 18:02:59 -0400

    chore: remove unused configuration and source files; clean up project structure

--- absolutejs-project/postcss.config.ts
+++
@@ -1,8 +0,0 @@
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
-
-const config = {
-	plugins: [tailwindcss, autoprefixer]
-};
-
-export default config;
--- absolutejs-project/src/frontend/styles/default/styleReset.ts
+++
@@ -1,8 +0,0 @@
-export const styleReset = `
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-weight: inherit;
-    }
-`;
