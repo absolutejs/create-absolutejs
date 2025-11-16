4c0d0b9b9ae034294f70df7965b3f2c0adcdcbe5
Benzi <benzi.villaruel@gmail.com>
2025-11-15 23:32:41 -0500

    fix: drizzle postgres import

--- src/generators/db/handlerTemplates.ts
+++ src/generators/db/handlerTemplates.ts
@@ -365,7 +365,7 @@ import { schema, type SchemaType } from '../../../db/schema'`,
 		dbType: 'NodePgDatabase<SchemaType>',
 		importLines: `
 import { eq } from 'drizzle-orm'
-import { NodePgDatabase } from 'drizzle-orm/postgres'
+import { NodePgDatabase } from 'drizzle-orm/node-postgres'
 import { schema, type SchemaType } from '../../../db/schema'`,
 		queries: drizzleQueryOperations
 	},
